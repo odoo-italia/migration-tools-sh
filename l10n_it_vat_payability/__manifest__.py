c8ed5a2a3b2e5f3a8ee277ecd3c30cdc8d9cf190
Borruso <gborruso@dinamicheaziendali.it>
2025-07-08 11:19:54 +0200

    [ADD] l10n_it_vat_payability: added skeleton module

---
+++ l10n_it_vat_payability/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2017 Alessandro Camilli - Openforce
+# Copyright 2018 Lorenzo Battistini <https://github.com/eLBati>
+# Copyright 2022 Marco Colombo <marco.colombo@phi.technology>
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "ITA - Esigibilità IVA",
+    "version": "18.0.1.0.1",
+    "development_status": "Beta",
+    "category": "Localization/Italy",
+    "author": "Openforce di Camilli Alessandro, Odoo Community Association (OCA)",
+    "website": "https://github.com/odoo-italia/migration-tools-sh",
+    "license": "LGPL-3",
+    "depends": [
+        "account",
+    ],
+    "data": [
+        "views/account_view.xml",
+    ],
+    "installable": True,
+}
