88f0a6d56844eaed5e7c4aad6c479dad9b0d7a5f
Borruso <gborruso@dinamicheaziendali.it>
2025-07-08 11:45:26 +0200

    [ADD] l10n_it_website_sale_fatturapa: added skeleton module

---
+++ l10n_it_website_sale_fatturapa/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2019 Simone Rubino
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Italian Localization - Fattura Elettronica - eCommerce",
+    "summary": "Aggiunge i campi necessari alla fatturazione elettronica "
+    "nel form del checkout",
+    "version": "18.0.1.0.0",
+    "category": "Localization/Italy",
+    "website": "https://github.com/odoo-italia/migration-tools-sh"
+    "/tree/14.0/l10n_it_website_sale_fatturapa",
+    "author": "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "depends": ["l10n_it_fatturapa", "l10n_it_website_sale_fiscalcode"],
+    "data": [
+        # "views/templates.xml"
+    ],
+    "auto_install": True,
+}
