c1194f54757cd1c7fd9c1bae94d6d20834d2dc92
Borruso <gborruso@dinamicheaziendali.it>
2025-07-08 10:57:04 +0200

    [ADD] l10n_it_fatturapa_out_sp: added skeleton module

---
+++ l10n_it_fatturapa_out_sp/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2020 Marco Colombo
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "ITA - Fattura elettronica - Emissione - Scissione Pagamenti",
+    "version": "18.0.1.0.1",
+    "development_status": "Beta",
+    "category": "Localization/Italy",
+    "summary": "Scissione pagamenti in fatturapa",
+    "author": "Marco Colombo," "Odoo Community Association (OCA)",
+    "website": "https://github.com/odoo-italia/migration-tools-sh",
+    "license": "AGPL-3",
+    "depends": [
+        "l10n_it_fatturapa_out",
+        "l10n_it_split_payment",
+    ],
+    "installable": True,
+    "auto_install": True,
+}
