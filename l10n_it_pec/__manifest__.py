55737084298fafcd150976ddd319b0706e5bf261
Borruso <gborruso@dinamicheaziendali.it>
2025-07-08 11:10:51 +0200

    [ADD] l10n_it_pec: added skeleton module

---
+++ l10n_it_pec/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2014 Associazione Odoo Italia (<http://www.odoo-italia.org>)
+# Copyright 2019 Stefano Consolaro (Associazione PNLUG - Gruppo Odoo)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "ITA - Email PEC",
+    "version": "18.0.1.0.1",
+    "category": "Localization/Italy",
+    "summary": "Aggiunge il campo email PEC al partner",
+    "author": "Odoo Italia Network,Odoo Community Association (OCA)",
+    "website": "https://github.com/odoo-italia/migration-tools-sh",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "data": [
+        # "views/partner_view.xml",
+    ],
+    "installable": True,
+}
